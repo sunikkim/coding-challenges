f2ca2b0850f6c6903cfc8be17c9bb97aebfd3fcd
Sunik Kim <sunik.h.kim@gmail.com>
2021-11-22 13:31:33 -0800

    numberOfIslands

---
+++ numberOfIslands.js
@@ -0,0 +1,52 @@
+/*
+Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+
+An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+Constraints:
+
+m == grid.length
+n == grid[i].length
+1 <= m, n <= 300
+grid[i][j] is '0' or '1'.
+
+---
+
+O number of islands in grid
+I grid of land and water
+C none
+E none
+
+basic idea: islands are contiguous "areas" of adjacent 1s. make recursive search helper function. keep track of visited locations with hash table and iterate through grid. if grid value is 1, increment counter by 1 and search in every direction from the value.
+*/
+
+var numIslands = function(grid) {
+  let count = 0;
+  const maxHeight = grid.length;
+  const maxWidth = grid[0].length;
+  const memo = {};
+
+  const search = (grid, i, j, memo) => {
+      if (i < 0 || i >= maxHeight || j < 0 || j >= maxWidth || grid[i][j] === '0' || `${i}?${j}` in memo) {
+          return;
+      }
+
+      memo[`${i}?${j}`] = true;
+
+      search(grid, i + 1, j, memo);
+      search(grid, i - 1, j, memo);
+      search(grid, i, j + 1, memo);
+      search(grid, i, j - 1, memo);
+  };
+
+  for (let i = 0; i < maxHeight; i++) {
+      for (let j = 0; j < maxWidth; j++) {
+          if (grid[i][j] === '1' && !memo[`${i}?${j}`]) {
+              count++;
+              search(grid, i, j, memo);
+          }
+      }
+  }
+
+  return count;
+};
